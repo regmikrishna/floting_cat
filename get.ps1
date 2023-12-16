f1c734185313e6115da274b9326dd58e9dfe8d3c
Krishna Regmi <127970029+thelegylegit@users.noreply.github.com>
2023-12-16 22:45:09 +0545

    Update get.ps1
    Signed-off-by: Krishna Regmi <127970029+thelegylegit@users.noreply.github.com>

--- get.ps1
+++ get.ps1
@@ -4,8 +4,8 @@ $ErrorActionPreference = "Stop"
 # Enable TLSv1.2 for compatibility with older clients
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://www.regmikrishna.com.np/win_office/Activation-Scripts/MAS_AIO.cmd'
-$DownloadURL2 = 'https://www.regmikrishna.com.np/backup/win_office/Activation-Scripts/MAS_AIO.cmd'
+$DownloadURL = "https://www.regmikrishna.com.np/win_office/Activation-Scripts/MAS_AIO.cmd"
+$DownloadURL2 = "https://www.regmikrishna.com.np/backup/win_office/Activation-Scripts/MAS_AIO.cmd"
 
 $rand = Get-Random -Maximum 99999999
 $isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
